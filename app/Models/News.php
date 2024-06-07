0bbfb9b1dd18d4bc7120606efbf3cdbee999688f
Valery Leen <vnwiz@ya.ru>
2024-06-07 14:38:53 +0400

    Version 1.0.1 - updated News model

--- app/Models/News.php
+++ app/Models/News.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+        'status',
+    ];
+
+    /**
+     * The user that the news belongs to.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
